9db203f693d2a4b79441b4cabd2680ec78e534e0
etl17 <etl17@users.noreply.github.com>
2015-03-13 17:34:15 -0700

    Update spark-env.sh

--- templates/root/spark/conf/spark-env.sh
+++ templates/root/spark/conf/spark-env.sh
@@ -15,7 +15,7 @@ export SPARK_SUBMIT_LIBRARY_PATH="$SPARK_SUBMIT_LIBRARY_PATH:/root/ephemeral-hdf
 export SPARK_SUBMIT_CLASSPATH="$SPARK_CLASSPATH:$SPARK_SUBMIT_CLASSPATH:/root/ephemeral-hdfs/conf"
 
 # Bind Spark's web UIs to this machine's public EC2 hostname:
-export SPARK_PUBLIC_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
+export SPARK_PUBLIC_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/local-hostname`
 
 # Set a high ulimit for large shuffles
 ulimit -n 1000000
